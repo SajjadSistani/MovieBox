729b2a1b00d5305c87e56dd1bd988a296b7842dd
Saji <sajjadsistani79@gmail.com>
2023-11-10 18:00:13 +0330

    Done

--- script.js
+++ script.js
@@ -52,6 +52,24 @@ const getData = async () => {
   }
 };
 
+const test = async () => {
+  const url = "https://deezerdevs-deezer.p.rapidapi.com/infos";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "d931af0261msh7820d8c821c3e5bp15b497jsn3431151f07dd",
+      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+};
 getData();
 
 searchBtn.addEventListener("click", getSearchedGenreData(searchInput.value));
